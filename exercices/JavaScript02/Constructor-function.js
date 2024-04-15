6b6d4812903911804eca56c3e5ae28c992b83ac8
Lucas Souza Diniz <lucas87diniz@gmail.com>
2024-04-15 20:10:36 -0300

    Update Constructor-function.js

--- exercices/JavaScript02/Constructor-function.js
+++ exercices/JavaScript02/Constructor-function.js
@@ -10,18 +10,71 @@
 
 //pratica
 
-function Dom() {
-  this.seletor = "li";
-  const element = document.querySelector(this.seletor);
-  this.ativo = function () {
-    element.classList.add("ativo");
+// function Dom() {
+//   this.seletor = "li";
+//   const element = document.querySelector(this.seletor);
+//   this.ativo = function () {
+//     element.classList.add("ativo");
+//   };
+// }
+
+// const lista = new Dom();
+// lista.seletor = "ul";
+// lista.ativo();
+
+// const lastLi = new Dom();
+// lastLi.seletor = "li:last-child";
+// lastLi.ativo();
+
+// Transforme o objeto abaixo em uma Constructor Function
+const pessoa = {
+  nome: "Nome pessoa",
+  idade: 0,
+  andar() {
+    console.log(this.nome + " andou");
+  },
+};
+
+function Pessoa(nome, idade) {
+  this.nome = nome;
+  this.idade = idade;
+  this.andar = function () {
+    console.log(nome + " Andou");
   };
 }
 
-const lista = new Dom();
-lista.seletor = "ul";
-lista.ativo();
+// Crie 3 pessoas, João - 20 anos,
+// Maria - 25 anos, Bruno - 15 anos
 
-const lastLi = new Dom();
-lastLi.seletor = "li:last-child";
-lastLi.ativo();
+const joao = new Pessoa("Joao", 20);
+const maria = new Pessoa("Maria", 25);
+const bruno = new Pessoa("Bruno", 15);
+
+// Crie uma Constructor Function (Dom) para manipulação
+// de listas de elementos do dom. Deve conter as seguintes
+// propriedades e métodos:
+//
+// elements, retorna NodeList com os elementos selecionados
+// addClass(classe), adiciona a classe a todos os elementos
+// removeClass(classe), remove a classe a todos os elementos
+
+function Dom(selector) {
+  const elementList = document.querySelectorAll(selector);
+  this.elements = elementList;
+  this.addClass = function (classe) {
+    elementList.forEach((element) => {
+      element.classList.add(classe);
+    });
+  };
+  this.removeCLass = function (classe) {
+    elementList.forEach((element) => {
+      element.classList.remove(classe);
+    });
+  };
+}
+
+const listaItens = new Dom("li");
+const ul = new Dom("ul");
+
+listaItens.addClass("active");
+ul.addClass("active-ul");
