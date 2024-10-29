6717e25321333b4e43b967a03c8763456bf0eefe
Lucas Souza Diniz <lucas87diniz@gmail.com>
2024-10-29 19:22:37 -0300

    new exercice

---
+++ exercices/JavaScript01/scripts/script12.js
@@ -0,0 +1,75 @@
+// Utilizando o foreach na array abaixo,
+// some os valores de Taxa e os valores de Recebimento
+
+// const transacoes = [
+//   {
+//     descricao: 'Taxa do Pão',
+//     valor: 39
+//   },
+//   {
+//     descricao: 'Taxa do Mercado',
+//     valor: 129
+//   },
+//   {
+//     descricao: 'Recebimento de Cliente',
+//     valor: 99
+//   },
+//   {
+//     descricao: 'Taxa do Banco',
+//     valor: 129
+//   },
+//   {
+//     descricao: 'Recebimento de Cliente',
+//     valor: 49
+//   },
+// ];
+
+
+// let taxaTotal = 0;
+// let recebimentoTotal = 0;
+
+// transacoes.forEach((item) => {
+//   if (item.descricao.slice(0,4) === 'Taxa') {
+//     taxaTotal += item.valor;
+//   }
+//   if (item.descricao.slice(0,4) === 'Rece') {
+//     recebimentoTotal += item.valor;
+//   }
+// });
+
+// console.log(taxaTotal)
+// console.log(recebimentoTotal)
+
+
+// Retorne uma array com a lista abaixo
+// const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+
+// const arrayTransportes = transacoes.split()
+// // Substitua todos os span's por a's
+// const html = `<ul>
+//                 <li><span>Sobre</span></li>
+//                 <li><span>Produtos</span></li>
+//                 <li><span>Contato</span></li>
+//               </ul>`;
+
+// // Retorne o último caracter da frase
+// const frase = 'Melhor do ano!';
+
+// // Retorne o total de taxas
+// const transacoes = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+
+// let taxasTotal = 0 
+
+// transacoes2.forEach(item=> {
+//   item = item.toLowerCase().trim();
+//   item = item.slice(0,4);
+
+
+
+//   if(item === 'taxa')
+//     taxasTotal++
+// });
+
+// console.log(taxasTotal)
+
+
