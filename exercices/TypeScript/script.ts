5a71348c974777a258c0c976e831f1477b9b8e74
Lucas Souza Diniz <lucas87diniz@gmail.com>
2024-12-26 21:20:14 -0300

    add Instanceof

--- exercices/TypeScript/script.ts
+++ exercices/TypeScript/script.ts
@@ -250,3 +250,54 @@ const games: Products1 = {
 
 games.nome05?.toLowerCase();
 libre.nome05?.toLowerCase();
+
+//Class
+
+//Quando definimos uma classe, o TypeScript gera a interface do objeto produzido pela mesma.
+
+class Produto {
+  nome: string;
+  preco: number;
+  constructor(nome: string, preco: number) {
+    this.nome = nome;
+    this.preco = preco;
+  }
+  precoReal() {
+    return `R$ ${this.preco}`;
+  }
+}
+const livro = new Produto("Prime", 65);
+
+//instanceof
+
+//podemos verificar se um objeto é uma instância (foi construído ou herda) de uma função construtora (class).
+
+class Livro {
+  autor: string;
+  constructor(autor: string) {
+    this.autor = autor;
+  }
+}
+
+class Jogo {
+  jogadores: number;
+  constructor(jogadores: number) {
+    this.jogadores = jogadores;
+  }
+}
+
+function buscarProduto(busca: string) {
+  if (busca === "O Hobbit") {
+    return new Livro("J. R. R. Tolkien");
+  }
+  if (busca === "Dark Souls") {
+    return new Jogo(1);
+  }
+  return null;
+}
+
+const produto = buscarProduto("O Hobbit");
+
+if (produto instanceof Livro) {
+  produto.autor;
+}
