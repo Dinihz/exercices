3154414d599f1c794416b95b6c0ffa26c2e69792
Lucas Souza Diniz <lucas87diniz@gmail.com>
2025-01-07 21:10:27 -0300

    add exercice 5

--- exercices/TypeScript/script.ts
+++ exercices/TypeScript/script.ts
@@ -317,3 +317,56 @@ function round(value: number | string) {
     return num.toString();
   }
 }
+
+// Type Guards
+
+// 1 - Faça um fetch da API: https://api.origamid.dev/json/cursos.json
+// 2 - Defina a interface da API
+// 3 - Crie um Type Guard, que garanta que a API possui nome, horas e tags
+// 4 - Use Type Guards para garantir a Type Safety do código
+// 5 - Preencha os dados da API na tela.
+
+//async function fetchCursos() {
+//   const response = await fetch("https://api.origamid.dev/json/cursos.json");
+//   const json = await response.json();
+//   handleCursos(json);
+// }
+// fetchCursos();
+
+interface Curso {
+  nome: string;
+  horas: number;
+  aulas: number;
+  gratuito: boolean;
+  tags: string[];
+  idAulas: number[];
+  nivel: "iniciante" | "avancado";
+}
+
+function isCurso(curso: unknown): curso is Curso {
+  if (
+    curso &&
+    typeof curso === "object" &&
+    "nome" in curso &&
+    "horas" in curso &&
+    "tags" in curso
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function handleCursos(data: unknown) {
+  if (Array.isArray(data)) {
+    data.filter(isCurso).forEach((item) => {
+      document.body.innerHTML += `
+        <div>
+          <h2>${item.nome}</h2>
+          <p>${item.horas}</p>
+          <p>${item.tags.join(", ")}</p>
+        </div>
+      `;
+    });
+  }
+}
