a6d8340cacfa622d9a0db4ab6865bd4a598b5459
Dinihz <lucas87diniz@gmail.com>
2025-02-05 22:28:19 -0300

    chore: added exercice

--- React/project-react/src/main.jsx
+++ React/project-react/src/main.jsx
@@ -3,3 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(<App />);
+// Mostre os dados da aplicação, como aprensetado no vídeo
+// Não utilize CSS externo, use o style para mudar as cores
+// Se a situação estiver ativa pinte de verde, inativa vermelho
+// Se o gasto for maior que 10000 mostre uma mensagem
