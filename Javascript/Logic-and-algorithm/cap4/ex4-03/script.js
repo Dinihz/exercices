5057a35b2ac7fbf10b428086c25569d1b038fd03
dinihz <lucas87diniz@gmail.com>
2025-03-17 20:18:55 -0300

    chore: add exercice 3 in cap4

---
+++ Javascript/Logic-and-algorithm/cap4/ex4-03/script.js
@@ -0,0 +1,14 @@
+const inDescription = document.getElementById("inDescription");
+
+const inValue = document.getElementById("inValue");
+
+const btRegistration = document.getElementById("btRegistration");
+
+const outList = document.getElementById("outList");
+
+const outTotal = document.getElementById("outTotal");
+
+function calcAccont() {
+  const description = inDescription.value;
+  const value = Number(inValue.value);
+}
