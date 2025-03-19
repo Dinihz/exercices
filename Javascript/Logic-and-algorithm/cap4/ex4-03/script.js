2e58450a3d56dda9605301c8501a14b040ef2dc6
dinihz <lucas87diniz@gmail.com>
2025-03-19 18:49:25 -0300

    refactor: improved the code

--- Javascript/Logic-and-algorithm/cap4/ex4-03/script.js
+++ Javascript/Logic-and-algorithm/cap4/ex4-03/script.js
@@ -1,3 +1,4 @@
+//Select elements DOM
 const inDescription = document.getElementById("inDescription");
 const inValue = document.getElementById("inValue");
 const btRegistration = document.getElementById("btRegistration");
@@ -6,40 +7,55 @@ const outTotal = document.getElementById("outTotal");
 const btAllClear = document.getElementById("btAllClear");
 const btClear = document.getElementById("btClear");
 
+//Global variable
+let numContas = 0;
 let listAccont = "";
 let totalValue = 0;
 
 function calcAccont() {
-  const description = inDescription.value;
+  const description = inDescription.value.trim();
   const value = Number(inValue.value);
 
-  listAccont += `${description}: R$ ${value.toFixed(2)}<br>`;
-  totalValue += value;
-
+  //Data validation
   if (description === "" || isNaN(value) || value <= 0) {
     alert("Please, put a valid number");
+    inDescription.focus();
+    return;
   }
 
-  outList.innerHTML = listAccont;
-  outTotal.innerHTML = `Total: R$ ${totalValue.toFixed(2)}`;
+  //increments the values
+  numContas++;
+  totalValue += value;
+  listAccont += `${description}: R$ ${value.toFixed(2)}<br>`;
 
+  //update and show in html
+  outList.innerHTML = listAccont + "<hr>";
+  outTotal.innerHTML = `${numContas} account(s) - Total: R$ ${totalValue.toFixed(2)}`;
+
+  //clean
   inDescription.value = "";
   inValue.value = "";
   inDescription.focus();
 }
 
+//clean inputs
 function clearButton() {
   inDescription.value = "";
   inValue.value = "";
 }
 
+//clean all
 function clearAllButton() {
+  numContas = 0;
   listAccont = "";
   totalValue = 0;
+
   outList.innerHTML = "";
-  outTotal.innerHTML = "";
+  outTotal.innerHTML = "No registered account";
+
   inDescription.value = "";
   inValue.value = "";
+  inDescription.focus();
 }
 
 btRegistration.addEventListener("click", calcAccont);
