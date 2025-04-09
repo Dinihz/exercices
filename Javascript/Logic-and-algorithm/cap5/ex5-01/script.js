919682a37ef409f64402f065c9cf6262c67cee54
dinihz <lucas87diniz@gmail.com>
2025-04-09 20:48:35 -0300

    chore: finished exercise 1 chapter 5

--- Javascript/Logic-and-algorithm/cap5/ex5-01/script.js
+++ Javascript/Logic-and-algorithm/cap5/ex5-01/script.js
@@ -1,3 +1,4 @@
+// get html elements
 const inputPatient = document.getElementById("inPatient");
 const buttonAdd = document.getElementById("btAdd");
 const buttonUrgency = document.getElementById("btUrgency");
@@ -5,25 +6,46 @@ const buttonAttend = document.getElementById("btAttend");
 const outResult = document.getElementById("outResult");
 const outArray = document.getElementById("outArray");
 
-const patient = inputPatient.value;
+const patientsArray = [];
 
-const patientsArray = [
-  "1. Lucas",
-  "2. Gilberto",
-  "3. Warley",
-  "4. Sergio",
-  "5. Marconde",
-];
+// display the array in the html
+function showArray() {
+  outArray.innerHTML = patientsArray
+    .map((p, i) => `${i + 1} - ${p}`) // using map to transform each item in an array into a nubered string
+    .join("<br>"); // join the transformed array into a single string, separating each item with a line break (<br>)
+  inputPatient.value = "";
+  inputPatient.focus();
+}
 
+// add new patient to the end of the array
 function addPatient() {
-  const pushPatient = patientsArray.push(patient);
+  const patient = inputPatient.value;
+  if (patient === "") return;
+  patientsArray.push(patient); // using push to go to the array
+  showArray();
 }
 
+// move the patient to the top if it's urgent
 function addUrgencyPatient() {
-  const patientUrgency = patientsArray.splice(indexOf(patient), 1)[0];
+  const patient = inputPatient.value;
+  if (patient === "") return;
+  patientsArray.unshift(patient); // using unshift to add to the top
+  showArray();
 }
 
+// remove the first patient from the array and display as in attendance
 function attendPatient() {
-  if()
+  if (patientsArray.length === 0) {
+    outResult.textContent = `No patient in the queue`;
+    return;
+  }
+
+  const nextPatient = patientsArray.shift(); // use shift to remove the first patient from the array
+  outResult.textContent = `In attendance ${nextPatient}`;
+  showArray();
 }
 
+// add event listeners to the buttons
+buttonAdd.addEventListener("click", addPatient);
+buttonUrgency.addEventListener("click", addUrgencyPatient);
+buttonAttend.addEventListener("click", attendPatient);
---
+++ TypeScript/logic-and-algorithm/cap1/ex5/script.ts
@@ -0,0 +1,4 @@
+const form = document.querySelector("form") as HTMLFormElement
+const inValue = document.querySelector("#inValue") as HTMLInputElement
+const inUse = document.querySelector("#inUse") as HTMLInputElement
+const outValue = document.querySelector("#outValue") as HTMLHeadingElement
