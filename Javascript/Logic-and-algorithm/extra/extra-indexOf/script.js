67a19d6213b1796ccff694b37792500532d73e18
dinihz <lucas87diniz@gmail.com>
2025-04-11 20:37:33 -0300

    chore add indexof study

---
+++ Javascript/Logic-and-algorithm/extra/extra-indexOf/script.js
@@ -0,0 +1,21 @@
+document.getElementById("outLoop").textContent = "Script funcionando!";
+
+const age = [16, 17, 18, 17, 20];
+
+console.log(age);
+
+const ageIndex = age.indexOf(17);
+
+console.log(ageIndex);
+
+const ageLast = age.lastIndexOf(17);
+
+console.log(ageLast);
+
+const ageTest = age.indexOf(19);
+
+console.log(ageTest);
+
+const ageIncludes = age.includes(20);
+
+console.log(ageIncludes);
