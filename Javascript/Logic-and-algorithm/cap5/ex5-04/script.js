96541da51831d6c07a89483156e0af7efa592db1
dinihz <lucas87diniz@gmail.com>
2025-06-23 19:08:28 -0300

    chore: add init of exercice 4 in chapter 5

---
+++ Javascript/Logic-and-algorithm/cap5/ex5-04/script.js
@@ -0,0 +1,6 @@
+const frm = document.querySelector("form");
+const answer = document.querySelector("pre");
+const imputName = document.querySelector("#inName");
+const inputAge = document.querySelector("#inAge");
+
+const child = [];
