88ca4e44bc7fd6913f7b265ecfcbcae42b2cc9e0
dinihz <lucas87diniz@gmail.com>
2025-06-24 18:00:53 -0300

    chore: add form function

--- Javascript/Logic-and-algorithm/cap5/ex5-04/script.js
+++ Javascript/Logic-and-algorithm/cap5/ex5-04/script.js
@@ -1,6 +1,21 @@
 const frm = document.querySelector("form");
 const answer = document.querySelector("pre");
-const imputName = document.querySelector("#inName");
+const inputName = document.querySelector("#inName");
 const inputAge = document.querySelector("#inAge");
+const btList = document.querySelector("#btList");
+const btFilter = document.querySelector("#btFilter");
 
 const child = [];
+
+frm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = inputName.value;
+  const age = Number(inputAge.value);
+
+  child.push({ name, age });
+  inputName.value = "";
+  inputAge.value = "";
+  inputName.focus();
+
+  btList.dispatchEvent(new Event("click"));
+});
