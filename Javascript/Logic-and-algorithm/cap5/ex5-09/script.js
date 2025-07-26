a5910d3829d4345d4013c32c1bd9d009ff6fe77d
dinihz <lucas87diniz@gmail.com>
2025-07-26 17:48:22 -0300

    chore: add init script.js

---
+++ Javascript/Logic-and-algorithm/cap5/ex5-09/script.js
@@ -0,0 +1,8 @@
+const inNumber = document.querySelector("#inNumber");
+const btAdd = document.querySelector("#btAdd");
+const btList = document.querySelector("#btList");
+
+const preNumbers = document.querySelector("#preNumbers");
+const preResult = document.querySelector("preResult");
+
+const num = [];
