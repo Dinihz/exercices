73356ae47375fea243b12fe78fbf39f849316f77
dinihz <lucas87diniz@gmail.com>
2025-07-29 18:20:55 -0300

    chore: fix  List button

--- Javascript/Logic-and-algorithm/cap5/ex5-09/script.js
+++ Javascript/Logic-and-algorithm/cap5/ex5-09/script.js
@@ -1,3 +1,4 @@
+//Elaborar um programa que adicione numeros a um vetor (OK). O programa deve impedir a inclusao de numeros repetidos. Exibir uma lista de numeros a cada inclusao. Ao clicar no botao Veficar ordem, o programa deve analisar o conteudo do vetor e informar se os numeros estao ou nao em ordem crescente.
 const inNumber = document.querySelector("#inNumber");
 const btAdd = document.querySelector("#btAdd");
 const btList = document.querySelector("#btList");
@@ -8,9 +9,13 @@ const preResult = document.querySelector("preResult");
 const nums = [];
 
 btAdd.addEventListener("click", () => {
-  const number = inNumber.value.trim();
-  if (number.length === 0) return;
+  const number = Number(inNumber.value.trim());
+  if (isNaN(number)) {
+    alert(`Please enter a valid number.`);
+  }
   nums.push(number);
   inNumber.value = "";
   inNumber.focus();
 });
+
+console.log(nums);
