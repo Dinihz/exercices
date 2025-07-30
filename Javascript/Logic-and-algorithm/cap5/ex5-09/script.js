c0f5ad31c9707a2bce673e0316c7a5fe5c367e91
dinihz <lucas87diniz@gmail.com>
2025-07-30 18:33:53 -0300

    chore: add preNumbers in exercice 9

--- Javascript/Logic-and-algorithm/cap5/ex5-09/script.js
+++ Javascript/Logic-and-algorithm/cap5/ex5-09/script.js
@@ -1,4 +1,4 @@
-//Elaborar um programa que adicione numeros a um vetor (OK). O programa deve impedir a inclusao de numeros repetidos (OK). Exibir uma lista de numeros a cada inclusao. Ao clicar no botao Veficar ordem, o programa deve analisar o conteudo do vetor e informar se os numeros estao ou nao em ordem crescente.
+//Elaborar um programa que adicione numeros a um vetor (OK). O programa deve impedir a inclusao de numeros repetidos (OK). Exibir uma lista de numeros a cada inclusao OK. Ao clicar no botao Veficar ordem, o programa deve analisar o conteudo do vetor e informar se os numeros estao ou nao em ordem crescente.
 const inNumber = document.querySelector("#inNumber");
 const btAdd = document.querySelector("#btAdd");
 const btList = document.querySelector("#btList");
@@ -14,7 +14,7 @@ btAdd.addEventListener("click", () => {
     alert(`Please enter a valid number.`);
   }
 
-  if (nums.includes(number) === true) {
+  if (nums.includes(number)) {
     alert(`This number has alredy been added.`);
     inNumber.value = "";
     return;
@@ -23,4 +23,6 @@ btAdd.addEventListener("click", () => {
   nums.push(number);
   inNumber.value = "";
   inNumber.focus();
+
+  preNumbers.textContent = `Numbers: ${nums.join(", ")}`;
 });
