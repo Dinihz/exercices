203026ecaa66986a6e69e4f46a1eab964da2640c
dinihz <lucas87diniz@gmail.com>
2025-08-05 12:44:42 -0300

    chore: added btAdd

--- Javascript/Logic-and-algorithm/cap5/ex5-10/script.js
+++ Javascript/Logic-and-algorithm/cap5/ex5-10/script.js
@@ -1,3 +1,7 @@
+/* Elaborar um programa que leia nome e número de acertos de candidatos inscritos em um concurso (ok). Listar os dados a cada inclusão. Ao clicar no botão Aprovados 2ª Fase, ler o número de acertos para aprovação dos candidatos para a 2ª fase do concurso, conforme ilustra a Figura 6.16. O programa deve, então, exibir os candidatos aprova-dos, ou seja, apenas os que obtiveram nota maior ou igual à nota informada. Exibir os candidatos aprovados em ordem decrescente de número de acertos (Figura 6.17). Caso nenhum candidato tenha sido aprovado, exibir mensagem.
+ *
+ * Develop a program that reads the names and number of correct answers of candidates enrolled in a contest. List the data for each entry. When clicking on the Approved 2nd Phase button, read the number of correct answers for the candidates' approval for the 2nd phase of the contest, as shown in Figure 6.16. The program should then display the approved candidates, i.e., only those who obtained a score greater than or equal to the specified score. Display the approved candidates in descending order of number of correct answers (Figure 6.17). If no candidates have been approved, display a message.*/
+
 const inCandidate = document.querySelector("#inCandidate");
 const inHits = document.querySelector("#inHits");
 const btAdd = document.querySelector("#btAdd");
@@ -6,3 +10,23 @@ const btApproved = document.querySelector("#btApproved");
 const preList = document.querySelector("#preList");
 
 const candidates = [];
+
+btAdd.addEventListener("click", () => {
+  const candidate = inCandidate.value;
+  const hits = Number(inHits.value.trim());
+
+  if (isNaN(hits)) {
+    alert(`Please enter a valid number`);
+  }
+
+  candidates.push({ name: candidate, hits: hits });
+  inCandidate.value = "";
+  inHits.value = "";
+  inCandidate.focus();
+
+  preList.textContent = candidates
+    .map((c, index) => `${index + 1}. ${c.name} - ${c.hits} hits`)
+    .join("\n");
+
+  console.log(candidates);
+});
