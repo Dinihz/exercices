fba66d4304a9be7554e50c239f1d6fb8468a8bb7
dinihz <lucas87diniz@gmail.com>
2025-08-05 11:47:51 -0300

    chore: add DOM manipulation

---
+++ Javascript/Logic-and-algorithm/cap5/ex5-10/script.js
@@ -0,0 +1,8 @@
+const inCandidate = document.querySelector("#inCandidate");
+const inHits = document.querySelector("#inHits");
+const btAdd = document.querySelector("#btAdd");
+const btList = document.querySelector("#btList");
+const btApproved = document.querySelector("#btApproved");
+const preList = document.querySelector("#preList");
+
+const candidates = [];
