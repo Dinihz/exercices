0a6e41f8633620118d4b447805d55c73316983cd
dinihz <lucas87diniz@gmail.com>
2025-08-13 19:05:41 -0300

    chore: add script

---
+++ Javascript/Logic-and-algorithm/cap6/ex06-03/script.js
@@ -0,0 +1,5 @@
+const inPasswd = document.querySelector("#inPassword");
+const btCheck = document.querySelector("#btCheck");
+const result = document.querySelector("#result");
+
+btCheck.addEventListener("click", () => {});
